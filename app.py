59cd3112fb569d86810e7223ff61d4151a1ab5fe
Daniel Mauricio Ceballos Gonzalez <dmceballosg@unal.edu.co>
2022-07-05 23:54:16 -0500

    Mostrando tabla ok

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
 from turtle import ht
 import dash
 import dash_auth
+import dash_table
 import dash_bootstrap_components as dbc
 import dash_html_components as html
+import requests
+import json
 from dash import dcc
 import base64
 import os
 from urllib.parse import quote as urlquote
 from flask import Flask, send_from_directory
+import pandas as pd
+from io import StringIO
+
 
 from dash import Input, Output, State, html
 from dash_bootstrap_components._components.Container import Container
@@ -37,7 +43,7 @@ search_bar = dbc.Row(
                     "Search", color="primary", className="ms-2", n_clicks=0
                 ),
                 width="auto",
-            )],style={"width":"auto"}),
+            )], style={"width": "auto"}),
     ],
     className="g-0 ms-auto flex-nowrap mt-3 mt-md-0 visible-menu",
     align="center",
@@ -159,6 +165,14 @@ list_group = html.Div(
     style={"minWidth": "100%"}
 )
 
+inputValid = [True, True, True, True]
+inputInvalid = [False, False, False, False]
+
+df_names = pd.DataFrame([{"cedula": "", "nombre1": "LUIS",
+                          "nombre2": "ANIBAL", "apellido1": "MOSQUERA", "apellido2": "MURILLO"}])
+
+# df.set_index('id', inplace=True, drop=False)
+
 tab1_content = dbc.Card(
     dbc.CardBody(
         [
@@ -166,25 +180,51 @@ tab1_content = dbc.Card(
                 [
                     dbc.Row(
                         [
-                            dbc.Col(html.Div([
-                                dcc.Input(id='name1', placeholder='Name 1', type='text'),
-                            ])),
-                            dbc.Col(html.Div([
-                                dcc.Input(id='name2', placeholder='Name 2', type='text'),
-                            ])),
-                            dbc.Col(html.Div([
-                                dcc.Input(id='lastname1', placeholder='LastName 1', type='text'),
-                            ])),
-                            dbc.Col(html.Div([
-                                dcc.Input(id='lastname2', placeholder='LastName 2', type='text')
-                            ])),
-                        ]
+                            dbc.Col(
+                                dbc.Input(id='name1', placeholder="Primer nombre", invalid=inputInvalid[0], valid=inputValid[0]), md=3),
+                            dbc.Col(
+                                dbc.Input(id='name2', placeholder="Segundo nombre", invalid=inputInvalid[1], valid=inputValid[1]), md=3),
+                            dbc.Col(
+                                dbc.Input(id='lastname1', placeholder="Primer apellido", invalid=inputInvalid[2], valid=inputValid[2]), md=3),
+                            dbc.Col(
+                                dbc.Input(id='lastname2', placeholder="segundo apellido", invalid=inputInvalid[3], valid=inputValid[3]), md=3),
+                        ], style={"width": "80%"},
                     ),
                     dbc.Row(
-                        html.Button(id='submit-button', type='submit', children='Submit')
+                        dbc.Button(id='submit-button',
+                                   type='submit', children='Submit',
+                                   color="primary", className="me-1", style={"marginTop": "20px", "marginBottom": "20px"})
                     ),
-                    html.Div(id='output_div')
-                ],
+                    html.Div([
+                        dash_table.DataTable(
+                            id='datatable-row-ids',
+                            data=df_names.to_dict('records'),
+                            style_cell_conditional=[
+                                {
+                                    'if': {'column_id': c},
+                                    'textAlign': 'center'
+                                } for c in ['nombre1', 'nombre2', 'apellido1', 'apellido2']
+                            ],
+                            style_data={
+                                'color': 'black',
+                                'backgroundColor': 'white'
+                            },
+                            style_data_conditional=[
+                                {
+                                    'if': {'row_index': 'odd'},
+                                    'backgroundColor': 'rgb(220, 220, 220)',
+                                }
+                            ],
+                            style_header={
+                                'backgroundColor': 'rgb(232 240 254)',
+                                'color': 'black',
+                                'fontWeight': 'bold'
+                            }
+                        ),
+                        html.Div(id='output_div')
+                    ])
+                    # html.Div(id='output_div')
+                ], style={"alignItems": "center", "display": "flex", "flexDirection": "column"}
             )
         ], className="search-name",
     ),
@@ -195,17 +235,6 @@ app = dash.Dash(external_stylesheets=[
                 dbc.themes.BOOTSTRAP, dbc.icons.BOOTSTRAP])
 app.title = "DS4A"
 
-@app.callback(Output('output_div', 'children'),
-                  Input('submit-button', 'n_clicks'),
-                  State('name1', 'value'),
-                  State('name2', 'value'),
-                  State('lastname1', 'value'),
-                  State('lastname2', 'value')
-                  )
-                  
-def update_output(clicks, name1, name2, lastname1, lastname2):
-        if clicks is not None:
-            return str(name1) + ' ' + str(name2) + ' ' + str(lastname1) + ' ' + str(lastname2)
 
 tab2_content = dbc.Card(
     dbc.CardBody(
@@ -257,6 +286,7 @@ UPLOAD_DIRECTORY_ND = "./upload_new_data"
 if not os.path.exists(UPLOAD_DIRECTORY_ND):
     os.makedirs(UPLOAD_DIRECTORY_ND)
 
+
 def download(path):
     """Serve a file from the upload directory."""
     return send_from_directory(UPLOAD_DIRECTORY_ND, path, as_attachment=True)
@@ -265,12 +295,12 @@ def download(path):
 upload = html.Div(
     [
         dcc.Upload(
-                id='upload-data',
-                children=html.Div([
+            id='upload-data',
+            children=html.Div([
                 'Drag and Drop or ',
                 html.A('Select Files')
-                ]),
-                style={
+            ]),
+            style={
                 'width': '400px',
                 'display': 'flex',
                 'justifyContent': 'center',
@@ -281,16 +311,17 @@ upload = html.Div(
                 'borderRadius': '5px',
                 'textAlign': 'center',
                 'margin': '10px'
-                },
-                multiple=True
-            ),
-            html.Div(id='output-data-upload'),
-            html.Div(dbc.Alert("Upload your file in csv format",
-            color="warning", className="alert"), className="alert-div"),
+            },
+            multiple=True
+        ),
+        html.Div(id='output-data-upload'),
+        html.Div(dbc.Alert("Upload your file in csv format",
+                           color="warning", className="alert"), className="alert-div"),
         html.H5("File List"),
         html.Ul(id="file-list"),
     ],
 )
+
 
 def save_file(name, content):
     """Decode and store a file uploaded with Plotly Dash."""
@@ -317,10 +348,9 @@ def file_download_link(filename):
 
 @app.callback(
     Output("file-list", "children"),
-    [Input("upload-data", "filename"), 
-    Input("upload-data", "contents")],
+    [Input("upload-data", "filename"),
+     Input("upload-data", "contents")],
 )
-
 def update_output(uploaded_filenames, uploaded_file_contents):
     """Save uploaded files and regenerate the file list."""
 
@@ -333,6 +363,7 @@ def update_output(uploaded_filenames, uploaded_file_contents):
         return [html.Li("No files yet!")]
     else:
         return [html.Li(file_download_link(filename)) for filename in files]
+
 
 # FOOTER
 footer = dbc.Container(
@@ -357,13 +388,14 @@ dinamicLayout = html.Div([
 ])
 
 app = dash.Dash(external_stylesheets=[
-                dbc.themes.BOOTSTRAP, dbc.icons.BOOTSTRAP],suppress_callback_exceptions=True)
+                dbc.themes.BOOTSTRAP, dbc.icons.BOOTSTRAP], suppress_callback_exceptions=True)
 app.title = "DS4A"
 
 auth = dash_auth.BasicAuth(
     app,
     VALID_USERNAME_PASSWORD_PAIRS
 )
+
 
 @app.callback(
     Output("navbar-collapse", "is_open"),
@@ -375,9 +407,9 @@ def toggle_navbar_collapse(n, is_open):
         return not is_open
     return is_open
 
+
 @app.callback(Output('page-content', 'children'),
               [Input('url', 'pathname')])
-
 def display_page(pathname):
     if pathname == '/upload_data':
         return upload
@@ -394,6 +426,34 @@ app.layout = dbc.Container(
     style={"maxWidth": "none", "padding": "0px"},
 )
 
+
+def validate_colums(elements_to_valid):
+    for key in range(0, len(elements_to_valid)):
+        if (not elements_to_valid[key]):
+            inputInvalid[key] = True
+        else:
+            inputValid[key] = False
+    return False
+
+
+@app.callback(Output('datatable-row-ids', 'data'),
+              Input('submit-button', 'n_clicks'),
+              State('name1', 'value'),
+              State('name2', 'value'),
+              State('lastname1', 'value'),
+              State('lastname2', 'value')
+              )
+def update_output(clicks, name1, name2, lastname1, lastname2):
+    if clicks is not None:
+        validate_colums([name1, name2, lastname1, lastname2])
+        dataDict = {'id': 1, 'name1': name1, 'name2': name2,
+                    'surname1': lastname1, 'surname2': lastname2}
+        r = requests.post('http://127.0.0.1:8050/search',
+                          data=json.dumps(dataDict))
+        r = json.load(StringIO(r.text))
+        df = pd.DataFrame(r)
+        return df.to_dict(orient='records')
+
+
 if __name__ == "__main__":
     app.run_server(debug=True, port=1234)
-
